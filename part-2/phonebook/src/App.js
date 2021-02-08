3f2c81388e1b7bcf423421c9afe89535143ec5d4
Walther <veeti.haapsamo@gmail.com>
2021-02-08 17:20:44 +0200

    Exercise 2.11

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { PersonForm, PersonFilterForm, PersonList } from "./Person";
 
 const App = () => {
-  // Default data from the course materials
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+
+  // fetch data from the server
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
