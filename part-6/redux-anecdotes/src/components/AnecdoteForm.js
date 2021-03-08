5827d8b53671f084a864d74c3c95eedf0b817328
Walther <veeti.haapsamo@gmail.com>
2021-03-08 15:52:19 +0200

    Exercises 6.3 - 6.8

---
+++ part-6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(createAnecdote(content));
+  };
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={(e) => addAnecdote(e)}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button>create</button>
+      </form>
+    </>
+  );
+};
+
+export default AnecdoteForm;
--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,37 @@ const reducer = (state = initialState, action) => {
   console.log("state now: ", state);
   console.log("action", action);
 
-  return state;
+  switch (action.type) {
+    case "VOTE":
+      const id = action.data.id;
+      const anecdoteToChange = state.find((n) => n.id === id);
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      };
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      );
+    case "NEW_ANECDOTE":
+      const newAnecdote = asObject(action.data.content);
+      return [...state, newAnecdote];
+    default:
+      return state;
+  }
+};
+
+export const createAnecdote = (content) => {
+  return {
+    type: "NEW_ANECDOTE",
+    data: { content },
+  };
+};
+
+export const voteAnecdote = (id) => {
+  return {
+    type: "VOTE",
+    data: { id },
+  };
 };
 
 export default reducer;
